8bb8a73be0bb1cf7c4e49cb667139b33c3cb971c
GitHub Action <action@github.com>
2021-04-11 15:07:04 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/jbardel/junitassertgeneratorintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jbardel.junitassertgeneratorintellijplugin.services
+
+import com.github.jbardel.junitassertgeneratorintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/jbardel/junitassertgeneratorintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jbardel/junitassertgeneratorintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jbardel.junitassertgeneratorintellijplugin.services
 
+import com.github.jbardel.junitassertgeneratorintellijplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
